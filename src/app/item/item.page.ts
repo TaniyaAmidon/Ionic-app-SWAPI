d533ad66e7ff464dcbfea9bcab8b1fbe71a1e228
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-03-27 22:47:19 +0000

    add pictures to items json

--- src/app/item/item.page.ts
+++ src/app/item/item.page.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import data from '../items.json';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.page.html',
   styleUrls: ['./item.page.scss'],
 })
+
 export class ItemPage implements OnInit {
-
-  constructor() { }
-
+  items :any
+  constructor() { 
+    this.items = data.items.id;
+    
+  }
+    
   ngOnInit() {
   }
 
