ced26f6bbb53232d205702cbb03a5ed45857bf6b
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-03-28 16:31:36 +0000

    add sidemenu links to item and list page

--- src/app/sidemenu/sidemenu.page.ts
+++ src/app/sidemenu/sidemenu.page.ts
@@ -8,23 +8,23 @@ import { Router, RouterEvent } from '@angular/router';
 })
 export class SidemenuPage implements OnInit {
 
-  pages = [
-    {
-      title: 'List',
-      url: '/list'
-    },
-    {
-      title: 'Item',
-      url: '/item'
-    }
-  ];
+  // pages = [
+  //   {
+  //     title: 'List',
+  //     url: '/list'
+  //   },
+  //   {
+  //     title: 'Item',
+  //     url: '/item'
+  //   }
+  // ];
 
-  selectedPath = '';
+  // selectedPath = '';
 
-  constructor(private router: Router) {
-    this.router.events.subscribe((event:RouterEvent) => {
-      this.selectedPath =  event.url;
-    });
+  constructor() {
+    // this.router.events.subscribe((event:RouterEvent) => {
+    //   this.selectedPath =  event.url;
+    // });
    }
 
   ngOnInit() {
